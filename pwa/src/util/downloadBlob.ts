15c36d7e430a3813bbc4607adb82655d117daa93
Martin Skalický <skalicky.martin@iotdomu.cz>
2026-01-06 10:42:19 +0100

    feat? add utf8 BOM to csv

--- pwa/src/util/downloadBlob.ts
+++ pwa/src/util/downloadBlob.ts
@@ -1,5 +1,8 @@
 export function downloadBlob(content: string, filename: string, contentType: string) {
-    const blob = new Blob([content], { type: contentType });
+    const blob = new Blob([
+        new Uint8Array([0xEF, 0xBB, 0xBF]), // UTF-8 BOM
+        content
+    ], { type: contentType });
     const url = URL.createObjectURL(blob);
 
     const pom = document.createElement('a');
